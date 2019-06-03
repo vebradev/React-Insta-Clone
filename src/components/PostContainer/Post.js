cb1f705d476a6b2da5e92284d34b94025f4a6500
Justinas Vebra <vebra@Justinass-MacBook-Pro.local>
2019-06-03 16:48:42 +0300

    Refactor small parts of Post container in Post.js

--- src/components/PostContainer/Post.js
+++ src/components/PostContainer/Post.js
@@ -1,14 +1,18 @@
 import React from "react";
+import CommentSection from "../CommentSection/CommentSection";
 
 const Post = props => {
   return (
     <div className="post">
       <div className="header">
-        {props.post.username}
         <img src={props.post.thumbnailUrl} alt="" />
+        <span className="username">{props.post.username}</span>
       </div>
       <div className="content">
         <img src={props.post.imageUrl} alt="" />
+      </div>
+      <div className="comments">
+          <CommentSection comments={props.post.comments}/>
       </div>
     </div>
   );
