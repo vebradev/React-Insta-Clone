da78083bf2500901b99a92cf3da7c3d6e065a7f8
Justinas Vebra <vebra@Justinass-MacBook-Pro.local>
2019-06-03 15:26:10 +0300

    Add PostContainer component to the App.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import dummyData from './dummy-data';
+import PostContainer from './components/PostContainer/PostContainer';
 import './App.css';
 
-function App() {
-  return (
-    <div>hello, insta</div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: dummyData,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <PostContainer posts={this.state.posts} />
+      </div>
+    );
+  }
 }
 
 export default App;
