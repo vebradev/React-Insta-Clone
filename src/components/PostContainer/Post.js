6d1ccb8b82bb9c4b257f6536b5d03ba40e08f79e
vebradev <justinas@vebra.dev>
2019-06-04 17:46:08 +0300

    Add functionality to likes button in Post component.

--- src/components/PostContainer/Post.js
+++ src/components/PostContainer/Post.js
@@ -1,24 +1,46 @@
 import React from "react";
 import CommentSection from "../CommentSection/CommentSection";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart, faComment } from '@fortawesome/free-solid-svg-icons'
 
-const Post = props => {
-  return (
-    <div className="post">
-      <div className="header">
-        <img src={props.post.thumbnailUrl} alt="" />
-        <span className="username">{props.post.username}</span>
+class Post extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      likes: this.props.likes,
+    };
+
+    this.hitLike = this.hitLike.bind(this);
+  }
+
+  hitLike = () => {
+    const likes = this.state.likes + 1;
+    this.setState({ likes });
+  }
+
+  render() {
+    return (
+      <div className="post">
+        <div className="header">
+          <img src={this.props.post.thumbnailUrl} alt="" />
+          <span className="username">{this.props.post.username}</span>
+        </div>
+        <div className="content">
+          <img src={this.props.post.imageUrl} alt="" />
+        </div>
+        <div className="controls">
+          <FontAwesomeIcon icon={faHeart} onClick={this.hitLike}/>
+          <FontAwesomeIcon icon={faComment} />
+        </div>
+        <div className="likes">
+          {this.state.likes} likes
+        </div>
+        <div className="comments">
+          <CommentSection comments={this.props.post.comments} />
+        </div>
       </div>
-      <div className="content">
-        <img src={props.post.imageUrl} alt="" />
-      </div>
-      <div className="likes">
-        {props.post.likes} likes
-      </div>
-      <div className="comments">
-          <CommentSection comments={props.post.comments}/>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Post;
