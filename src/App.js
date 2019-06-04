9960c08f92c361d12c3904adde4e6b1a3474fe29
vebradev <justinas@vebra.dev>
2019-06-04 14:50:07 +0300

    Set data to component's state in App.js

--- src/App.js
+++ src/App.js
@@ -8,8 +8,12 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      posts: dummyData,
+      posts: [],
     };
+  }
+
+  componentDidMount() {
+    this.setState({ posts: dummyData });
   }
 
   render() {
