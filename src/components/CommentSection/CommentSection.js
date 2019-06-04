2b9c33c7bc8412bfa62636a2467aa284ca7e1e20
vebradev <justinas@vebra.dev>
2019-06-04 15:00:02 +0300

    Refactor CommentSection component to class component & set it's own state for comments.

--- src/components/CommentSection/CommentSection.js
+++ src/components/CommentSection/CommentSection.js
@@ -1,19 +1,35 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
-const CommentSection = props => {
-  return (
-    <>
-      {props.comments.map(comment => (
-        <span key={comment.id}>
-          <span className="username">{comment.username}</span>
-          {comment.text}
-        </span>
-      ))}
-      <div className="add-comment">
-        <input placeholder="Add a comment..." />
-      </div>
-    </>
-  );
+class CommentSection extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      comments: props.comments
+    };
+  }
+
+  render() {
+    return (
+      <>
+        {this.props.comments.map(comment => (
+          <span key={comment.id}>
+            <span className="username">{comment.username}</span>
+            {comment.text}
+          </span>
+        ))}
+        <div className="add-comment">
+          <input placeholder="Add a comment..." />
+        </div>
+      </>
+    );
+  }
+}
+
+CommentSection.propTypes = {
+  comments: PropTypes.arrayOf(
+    PropTypes.shape({ text: PropTypes.string, username: PropTypes.string })
+  )
 };
 
 export default CommentSection;
