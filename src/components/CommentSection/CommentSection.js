f5aa0e98ea7f64f89513f30b01cbd8cac3bf7b42
vebradev <justinas@vebra.dev>
2019-06-04 17:45:23 +0300

    Add commenting functionallity in CommentSection component.

--- src/components/CommentSection/CommentSection.js
+++ src/components/CommentSection/CommentSection.js
@@ -1,26 +1,51 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import uuid from "uuid";
+import PropTypes from "prop-types";
 
 class CommentSection extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      comments: props.comments
+      comments: this.props.comments,
+      newComment: ""
     };
+
+    this.changeHandler = this.changeHandler.bind(this);
+    this.addNewComment = this.addNewComment.bind(this);
   }
+
+  changeHandler = e => {
+    this.setState({ newComment: e.target.value });
+  };
+
+  addNewComment = e => {
+    e.preventDefault();
+    this.setState({
+      comments: this.state.comments.concat({
+        id: uuid(),
+        username: "root",
+        text: this.state.newComment
+      }),
+      newComment: ""
+    });
+  };
 
   render() {
     return (
       <>
-        {this.props.comments.map(comment => (
+        {this.state.comments.map(comment => (
           <span key={comment.id}>
             <span className="username">{comment.username}</span>
             {comment.text}
           </span>
         ))}
-        <div className="add-comment">
-          <input placeholder="Add a comment..." />
-        </div>
+        <form className="add-comment" onSubmit={this.addNewComment}>
+          <input
+            placeholder="Add a comment..."
+            onChange={this.changeHandler}
+            value={this.state.newComment}
+          />
+        </form>
       </>
     );
   }
