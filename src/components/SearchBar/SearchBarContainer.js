24dd2ee6b36a3b6eecf036a25e1ea66e2a7b4836
vebradev <justinas@vebra.dev>
2019-06-05 17:15:49 +0300

    Add icons to SearchBarContainer.

--- src/components/SearchBar/SearchBarContainer.js
+++ src/components/SearchBar/SearchBarContainer.js
@@ -1,25 +1,37 @@
-import React from 'react';
-import Logo from '../../assets/inLogo.png';
-import Camera from '../../assets/camera.png';
+import React from "react";
+import Logo from "../../assets/inLogo.png";
+import Camera from "../../assets/camera.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCompass,
+  faHeart,
+  faUser
+} from "@fortawesome/free-regular-svg-icons";
 
-const SearchBarContainer = () => {
-    return (
-        <div className="search-bar-container">
-            <div className="logos">
-                <img className="camera-icon" src={Camera} alt="Camera icon"/>
-                <div></div>
-                <img className="instagram-logo" src={Logo} alt="Instagram Clone Logo"/>
-            </div>
-            <div className="search-bar">
-                <input placeholder="Search"/>
-            </div>
-            <div className="searc-bar-controls">
-                <span>compass</span>
-                <span>heart</span>
-                <span>user</span>
-            </div>
-        </div>
-    );
-}
+const SearchBarContainer = props => {
+  return (
+    <div className="search-bar-container">
+      <div className="logos">
+        <img className="camera-icon" src={Camera} alt="Camera icon" />
+        <div />
+        <img className="instagram-logo" src={Logo} alt="Instagram Clone Logo" />
+      </div>
+
+      <div className="search-bar">
+        <input
+          onChange={props.searchHandler}
+          value={props.searchQuery}
+          placeholder="Search"
+        />
+      </div>
+
+      <div className="search-bar-controls">
+        <FontAwesomeIcon icon={faCompass} />
+        <FontAwesomeIcon icon={faHeart} />
+        <FontAwesomeIcon icon={faUser} />
+      </div>
+    </div>
+  );
+};
 
 export default SearchBarContainer;
